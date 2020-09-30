998700af1e0134e7bdf953e0fe572b4d81776a66
Mr <lsjsss@live.cn>
2020-09-30 08:48:03 +0800

    9.30 class

---
+++ day3/snake.py
@@ -0,0 +1,38 @@
+# 1..定义游戏的主函数
+def main():
+    pass
+
+
+# 定义游戏的运行函数
+def runGame():
+    pass
+
+
+# 在游戏的右下角绘制显示消息
+def drawPressKeyMsg():
+    pass
+
+
+# 在游戏的右下角绘制显示消息
+def drawPressKeyMsg():
+    pass
+
+
+# 检查按键抬起事件
+def checkForKeyPress():
+    pass
+
+
+# 显示游戏刚进入时的欢迎界面
+def showStartScreen():
+    pass
+
+
+# 终止游戏程序
+
+
+
+
+
+
+
