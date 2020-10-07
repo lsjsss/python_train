25ae8cac0993c8857cfada2e4a539491e9a354c2
Mr <lsjsss@live.cn>
2020-10-07 08:02:20 +0800

    10.5 class

--- day6/face_model_1.py
+++ day6/face_model_1.py
@@ -2,7 +2,7 @@
 import os
 
 # 导入 numpy 模块，用于做矩阵的运算
-import numpy
+import numpy as np
 
 # 导入 cv 模块
 import cv2 as cv
@@ -23,15 +23,76 @@ def read_data(dirctory):
     faces_dir = {}
 
     # curdir 表示当前目录， subdir 表示当前目录下的所有子目录
-    for cur,subdir,files, in os.walk(dirctory):
+    for curdir,subdir,files, in os.walk(dirctory):
+        for jpeg in (
+            file for file in files if file.endswith(".jpg")
+        ):
+            path = os.path.join(curdir, jpeg)
+            print("path:", path)
 
+            # 获取文件标签
+            label = path.split(os.path.sep)
+            label = label[-2]
+            print(label)
+            if label not in faces_dir:
+                faces_dir[label] = []
+            else:
+                faces_dir[label].append(path)
 
+    # 数据预处理
+    codec = sp.LabelEncoder()
+    codec.fit(list(faces_dir.keys()))
 
+    x,y,z = [],[],[]
+    for label,filenames in faces_dir.items():
+        for filename in filenames:
+            # 局部二值模式，只能处理灰度图，进行灰度的转换
+            bgr = cv.imread(filename)
+            gray = cv.cvtColor(bgr, cv.COLOR_BGR2GRAY)
+            faces = face_detector.detectMultiScale(gray, 1.1, 2, minSize=(100, 100))
 
+            for l, t, w, h in faces:
+                a, b = int(w/2), int(h/2)
+                x.append(gray[t:t+h, l:l+w])
+                y.append(int(codec.transform([label])[0]))
+                cv.ellipse(bgr, (l+a, t+b), (a, b), 0, 0, 360, (255, 0, 255), 2)
+                z.append(bgr)
+    y = np.array(y)
+    return codec, x, y, z
 
+codec, train_x, train_y, train_z = read_data("faces\\training")
+# print("codec", codec)
+# print("*"*20)
+# print("train_x", train_x)
+# print("*"*20)
+# print("train_y", train_y)
+# print("*"*20)
+# print("train_z", train_z)
 
+_, test_x, test_y, test_z = read_data("faces\\testing")
 
+# 用局部二值模式直方图建立人脸识别模型
+model = cv.face.LBPHFaceRecognizer_create()
+model.train(train_x, train_y)
+pred_test_y = []
+for x in test_x:
+    y = model.predict(x)
+    print(y)
+    pred_test_y.append(y[0])
 
+escape = False
+while not escape:
+    for code, pred_code, image in zip(test_y,pred_test_y, test_z):
+        label = codec.inverse_transform([code])
+        print("label:", label)
+        label = label[0]
+        pred_label = codec.inverse_transform([pred_code])
+        print("pred_label:", pred_label)
 
+        text = "{}{}{}".format(label, "==" if label == pred_label else "!=", pred_label)
+        cv.putText(image, text, (10, 60), cv.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 6)
+        cv.imshow("Recognizing Face....", image)
 
-
+        if cv.waitKey(1000) == 27:
+            escape = True
+            break
