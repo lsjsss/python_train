ee357d37b1f811aac3d0308f341ca81f2d09f97e
Mr <lsjsss@live.cn>
2020-10-08 08:28:32 +0800

    10.6 class

--- day6/face_model_1.py
+++ day6/face_model_1.py
@@ -13,6 +13,7 @@ import sklearn.preprocessing as sp
 # 建立一个人脸追踪器
 face_detector = cv.CascadeClassifier("haar\\face.xml")
 
+
 # 定义方法，来读取图片文件
 def read_data(dirctory):
     # 判断是否为文件夹
@@ -23,19 +24,23 @@ def read_data(dirctory):
     faces_dir = {}
 
     # curdir 表示当前目录， subdir 表示当前目录下的所有子目录
-    for curdir,subdir,files, in os.walk(dirctory):
+    for curdir, subdir, files, in os.walk(dirctory):
         for jpeg in (
-            file for file in files if file.endswith(".jpg")
+                file for file in files if file.endswith(".jpg")
         ):
             path = os.path.join(curdir, jpeg)
+            # 读取图片路径
             print("path:", path)
 
             # 获取文件标签
             label = path.split(os.path.sep)
+            # print(label)
             label = label[-2]
-            print(label)
+
+            # 如果字典当中没有 label 键，就创建一个新的 label 为键的字典
             if label not in faces_dir:
-                faces_dir[label] = []
+                # 读取每个文件夹中的第一个文件
+                faces_dir[label] = [path]
             else:
                 faces_dir[label].append(path)
 
@@ -43,8 +48,8 @@ def read_data(dirctory):
     codec = sp.LabelEncoder()
     codec.fit(list(faces_dir.keys()))
 
-    x,y,z = [],[],[]
-    for label,filenames in faces_dir.items():
+    x, y, z = [], [], []
+    for label, filenames in faces_dir.items():
         for filename in filenames:
             # 局部二值模式，只能处理灰度图，进行灰度的转换
             bgr = cv.imread(filename)
@@ -52,13 +57,14 @@ def read_data(dirctory):
             faces = face_detector.detectMultiScale(gray, 1.1, 2, minSize=(100, 100))
 
             for l, t, w, h in faces:
-                a, b = int(w/2), int(h/2)
-                x.append(gray[t:t+h, l:l+w])
+                a, b = int(w / 2), int(h / 2)
+                x.append(gray[t:t + h, l:l + w])
                 y.append(int(codec.transform([label])[0]))
-                cv.ellipse(bgr, (l+a, t+b), (a, b), 0, 0, 360, (255, 0, 255), 2)
+                cv.ellipse(bgr, (l + a, t + b), (a, b), 0, 0, 360, (255, 0, 255), 2)
                 z.append(bgr)
     y = np.array(y)
     return codec, x, y, z
+
 
 codec, train_x, train_y, train_z = read_data("faces\\training")
 # print("codec", codec)
@@ -82,7 +88,7 @@ for x in test_x:
 
 escape = False
 while not escape:
-    for code, pred_code, image in zip(test_y,pred_test_y, test_z):
+    for code, pred_code, image in zip(test_y, pred_test_y, test_z):
         label = codec.inverse_transform([code])
         print("label:", label)
         label = label[0]
