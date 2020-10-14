6178ddd905bfa72a1f379af4f8797dbfb2e1bbc7
Mr <lsjsss@live.cn>
2020-10-14 08:38:58 +0800

    project 2

--- project/2_Fuzzy.py
+++ project/2_Fuzzy.py
@@ -1,30 +1,31 @@
 import face_recognition
 import cv2
+import cv2 as cv
 
 '''
 这是视频中人脸模糊的演示。请注意：此示例要求仅从网络摄像头读取时安装OpenCV（“ cv2”库）。
 使用face_recognition库*不需要* OpenCV。仅当您要运行此特定演示时才需要。
 如果您在安装它时遇到问题，请尝试其他不需要它的演示。获取对网络摄像头＃0的引用（默认值）
-
 '''
+
 video_capture = cv2.VideoCapture(0)
 
-# 初始化一些变量
+# 初始化面部位置变量
 face_locations = []
 
 while True:
-    # 抓取一帧视频
+    # 从视频中抓取一帧图像
     ret, frame = video_capture.read()
 
-    # 将视频帧调整为1/4尺寸以加快人脸检测处理
+    # 调整图像大小，将视频帧调整为 1/4 尺寸以加快人脸检测处理
     small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
     # 查找当前视频帧中的所有面部和面部编码
     face_locations = face_recognition.face_locations(small_frame, model="cnn")
 
-    # 显示结果
+    # 显示模糊后的效果
     for top, right, bottom, left in face_locations:
-        # 由于我们检测到的帧被缩放为1/4尺寸，因此应放大后脸的位置
+        # 由于检测到的帧被缩放为1/4尺寸，因此应放大后脸的位置，还原图像
         top *= 4
         right *= 4
         bottom *= 4
@@ -33,17 +34,17 @@ while True:
         # 提取图像中包含面部的区域
         face_image = frame[top:bottom, left:right]
 
-        # 模糊人脸图像
+        # 使用高斯模糊模糊人脸图像
         face_image = cv2.GaussianBlur(face_image, (99, 99), 30)
 
-        # 将模糊的脸部区域放回框架图像中
+        # 将模糊后的脸部区域放回图像中
         frame[top:bottom, left:right] = face_image
 
     # 显示结果图像
     cv2.imshow('Video', frame)
 
-    # 按键盘上的“ q”退出！
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    # 设置退出机制，按下ESC退出程序
+    if cv.waitKey(33) == 27:
         break
 
 # 释放手柄到网络摄像头
