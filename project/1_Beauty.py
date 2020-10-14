02ddb9ddfdf50c404208ae460c1931aa14806c3f
Mr <lsjsss@live.cn>
2020-10-14 08:22:30 +0800

    project

--- project/1_Beauty.py
+++ project/1_Beauty.py
@@ -1,34 +1,39 @@
 from PIL import Image, ImageDraw
 import face_recognition
 
-# 将 jpg 文件加载到 numpy 数组中,读取相片文件
-image = face_recognition.load_image_file("F:\\PycharmProject\\python_train-2020.9\\day8\\faces_data\\zhuyue2.jpg")
+# 加载图片，将 jpg 文件加载到 numpy 数组中
+image = face_recognition.load_image_file("F:\\PycharmProject\\python_train-2020.9\\day8\\faces_data\\zwq1.jpg")
 
 # 查找图像中所有面部的所有面部特征
 face_landmarks_list = face_recognition.face_landmarks(image)
 
+# 从导出阵列接口的对象创建一个图像存储器
 pil_image = Image.fromarray(image)
 for face_landmarks in face_landmarks_list:
     d = ImageDraw.Draw(pil_image, 'RGBA')
 
-    # 把眉毛变成噩梦
+    # 画眉毛轮廓
     d.polygon(face_landmarks['left_eyebrow'], fill=(68, 54, 39, 128))
     d.polygon(face_landmarks['right_eyebrow'], fill=(68, 54, 39, 128))
+
+    # 填充眉毛
     d.line(face_landmarks['left_eyebrow'], fill=(68, 54, 39, 150), width=5)
     d.line(face_landmarks['right_eyebrow'], fill=(68, 54, 39, 150), width=5)
 
-    # 唇彩
+    # 唇彩轮廓
     d.polygon(face_landmarks['top_lip'], fill=(150, 0, 0, 128))
     d.polygon(face_landmarks['bottom_lip'], fill=(150, 0, 0, 128))
+    # 填充唇彩
     d.line(face_landmarks['top_lip'], fill=(150, 0, 0, 64), width=8)
     d.line(face_landmarks['bottom_lip'], fill=(150, 0, 0, 64), width=8)
 
-    # 闪闪发光的眼睛
+    # 眼睛轮廓
     d.polygon(face_landmarks['left_eye'], fill=(255, 255, 255, 30))
     d.polygon(face_landmarks['right_eye'], fill=(255, 255, 255, 30))
 
-    # 涂一些眼线笔
+    # 绘制眼线
     d.line(face_landmarks['left_eye'] + [face_landmarks['left_eye'][0]], fill=(0, 0, 0, 110), width=6)
     d.line(face_landmarks['right_eye'] + [face_landmarks['right_eye'][0]], fill=(0, 0, 0, 110), width=6)
 
+    # 显示图像
     pil_image.show()
